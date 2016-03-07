97fb42a2fc486bbe47aeb0d13e1f700891801874
rcauchy <devhdmail@gmail.com>
2016-03-07 09:31:29 +0100

    ajout de dagger

--- MMSongQuizz/app/src/main/java/models/Song.java
+++ MMSongQuizz/app/src/main/java/models/Song.java
@@ -4,32 +4,15 @@ package models;
  * Created by remy on 07/03/2016.
  */
 public class Song {
+    private int id;
     private String title;
-    private String artisit;
 
 
     public Song(){
     }
 
-    public Song(String title, String artist){
-        this.title = title;
-        this.artisit = artist;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Song(String title){
         this.title = title;
     }
 
-    public String getArtisit() {
-        return artisit;
-    }
-
-    public void setArtisit(String artisit) {
-        this.artisit = artisit;
-    }
 }
